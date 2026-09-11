a5132cdc7236260b28db1bf08c7f1eb08416a42d
Новгородова Валерия <98082443+leranova02@users.noreply.github.com>
2026-09-11 12:55:31 +0900

    Update data.js

--- js/data.js
+++ js/data.js
@@ -214,7 +214,7 @@ window.materials = [
   {
     title: 'ВПР по математике — демо',
     desc: 'Демонстрационный вариант 2026',
-    file: 'files/vpr-math.pdf',
+    file: 'images/Впр1.pdf',
     tag: 'vpr',
     isNew: true               // это «новое», появится бейдж
   },
